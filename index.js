f18052217a26091d6f4fef1485e50ad597ae9531
LET <bjs.6300.137@gmail.com>
2024-02-21 12:06:06 +0900

    second commit

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ require("dotenv").config();
 console.log(process.env.API_KEY1);
 console.log(process.env.API_KEY2);
 console.log(process.env.API_KEY3);
+console.log(process.env.API_KEY4);
+console.log(process.env.API_KEY5);
